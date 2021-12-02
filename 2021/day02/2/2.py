e823773d35d54778a0333917ef1ec1a545a3bf58
Andrew Howard <andrew.howard@rackspace.com>
2021-12-02 10:26:31 -0600

    d2p2 done

---
+++ 2021/day02/2/2.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+  
+import sys
+import re
+
+# Debug function
+def debug(output):
+  print(output)
+#end debug()
+
+# CLI arg, input file
+if len(sys.argv) != 2:
+  print("ERR: Expect =1 arg; the file containing input")
+  exit(1)
+#end if
+try:
+  with open(sys.argv[1], 'r') as input:
+    lines = input.readlines()
+except:
+  print("ERR: Failed to open input file (%s)" % sys.argv[1])
+  exit(1)
+#end try
+
+
+#
+# The Meat
+
+xPos = 0
+yPos = 0
+val = 0
+aim = 0
+
+for line in lines:
+  val = int(re.search(r'\d+', line).group())
+  if "forward" in line:
+    xPos += val
+    yPos = yPos + aim*val
+    debug("Forward by " + str(val))
+  elif "up" in line:
+    aim -= val
+    debug("Up by " + str(val))
+  elif "down" in line:
+    aim += val
+    debug("Down by " + str(val))
+  else:
+    print("ERR: Unknown line")
+  #end if
+#end for
+
+print(xPos, ", ", yPos)
+print(xPos * yPos)
