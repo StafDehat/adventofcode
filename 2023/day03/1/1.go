3ea72185feb32ea09ca6ab30e0b5f84867f8954b
Andrew Howard <andrew.howard@rackspace.com>
2023-12-15 14:30:13 -0600

    d3p2p WIP

--- 2023/day03/1/1.go
+++ 2023/day03/1/1.go
@@ -47,56 +47,5 @@ func main() {
   /** Good god, finally down to actual logic **/
   //
 
-  var items []rune
-  for _,line := range lines {
-    items = append(items, findItem(line))
-  }
 
-  fmt.Print("Common items:",items,"(")
-  for _,item := range items {
-    fmt.Printf("%c,", item)
-  }
-  fmt.Println(")")
-
-  sum := 0
-  for _,item := range items {
-    sum += getPriority(item)
-  }
-  fmt.Println("Priority sum: ", sum)
 }
-
-func findItem(contents string) rune {
-  pocket1,pocket2 := getPockets(contents)
-  fmt.Println("Pocket1: ", pocket1)
-  fmt.Println("Pocket2: ", pocket2)
-  return getCommonChar(pocket1,pocket2)
-}
-
-func getPockets(contents string) (string,string) {
-  size := len(contents)
-  half := size / 2
-  return contents[0:half],contents[half:size]
-}
-
-func getCommonChar(pocket1 string, pocket2 string) rune {
-  // Ugh, if Go had sets, this would just be intersection(pocket1,pocket2)
-  counter := make(map[rune]int)
-  for _,char := range pocket1 {
-    counter[char] += 1
-  }
-  for _,char := range pocket2 {
-    if counter[char] > 0 {
-      return char
-    }
-  }
-  return 0
-}
-
-func getPriority(char rune) int {
-  // Shift ASCII values to 1-26,27-52
-  if char >= 'a' {
-    return int(char-96)
-  }
-  return int(char-38)
-}
-
